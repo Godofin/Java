e2b01b97cf458b85ea1ae911125ba5d9c38af7ce
Guilherme Lavezzo <65193596+Godofin@users.noreply.github.com>
2022-02-27 15:18:26 -0300

    Add files via upload

---
+++ ConhecendoALinguagem/TestaConvesao.java
@@ -0,0 +1,13 @@
+
+public class TestaConvesao {
+	public static void main(String[] args) {
+		double salario = 1270.5;
+		int valor = (int)salario;
+		System.out.println(valor);
+		
+		long numeroGrande = 320301531103050l;
+		short valorPequeno = 2123;
+		byte b = 120;
+		float pontoFlutuante = 3.14f;
+	}
+}
